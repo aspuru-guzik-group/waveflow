bb6bc65482e9b56153379af517329f933c8b024a
Chong Sun <sunchong137@gmail.com>
2024-07-02 15:25:28 -0500

    oops small fix

--- waveflow/benchmark_tests.py
+++ waveflow/benchmark_tests.py
@@ -108,7 +108,7 @@ def train_model(inputs, num_epochs, n_model_sample, model_type='IFlow',
     data_save_dir = f"{save_dir}/{dataset_name}/{model_type}_{spline_reg}"
     output_dir = f"{data_save_dir}/outputs/"
     if os.path.exists(output_dir):
-        formatted_datetime = now.strftime("%B-%d-%H")
+        formatted_datetime = datetime.now().strftime("%B-%d-%H")
         output_dir = f"{data_save_dir}/outputs/{formatted_datetime}"
     Path(output_dir).mkdir(parents=True, exist_ok=True)
     for epoch in range(1, num_epochs+1):
