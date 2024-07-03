7ad1c0c6b5918b2b60d16561726e5fb107e4acb3
Chong Sun <sunchong137@gmail.com>
2024-07-02 20:56:23 -0500

    benchmark test

--- waveflow/benchmark_tests.py
+++ waveflow/benchmark_tests.py
@@ -109,7 +109,7 @@ def train_model(inputs, num_epochs, n_model_sample, model_type='IFlow',
     kde_kl_divergences = []
     kde_hellinger_distances = []
     reconstruction_distances = []
-    data_save_dir = f"{save_dir}/{dataset_name}/{model_type}_{spline_reg}_{num_flow_layer}_{num_knots}/"
+    data_save_dir = f"{save_dir}/{dataset_name}/{model_type}_{spline_reg}_{num_flow_layer}_{spline_degree}_{num_knots}/"
     output_dir = f"{data_save_dir}/outputs/"
     if os.path.exists(output_dir):
         formatted_datetime = datetime.now().strftime("%M-%D-%H")
